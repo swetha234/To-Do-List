7db3d7218ae9b3f89c0cffea72d9f5e9e612572e
swetha234 <hgutt001@odu.edu>
2019-12-17 18:50:44 -0500

    2changes

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -3,20 +3,24 @@ import PropTypes from 'prop-types';
 
 export class TodoItem extends Component {
   getStyle = () => {
-    if (this.props.todo.completed) {
-      return {
-        textDecoration: 'line-through'
-      };
-    } else {
-      return {
-        textDecoration: 'none'
-      };
-    }
+    return {
+      background: '#f4f4f4',
+      padding: '2px',
+      borderBottom: '1px #ccc dotted',
+      textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+    };
+  };
+
+  markComplete = e => {
+    console.log(this.props);
   };
   render() {
     return (
       <div style={this.getStyle()}>
-        <p>{this.props.todo.title}</p>
+        <p>
+          <input type='checkbox' onChange={this.markComplete} />{' '}
+          {this.props.todo.title}
+        </p>
       </div>
     );
   }
